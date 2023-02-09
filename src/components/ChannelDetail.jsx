ecb9e49ac11b9e6dda881a1331331b96737703a1
vitheshshetty00 <vitheshshetty23@gmail.com>
2023-02-09 08:51:25 +0530

    finished Feed

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const ChannelDetail = () => {
+	const { id } = useParams();
+	const [channelDetail, setChannelDetail] = useState(null);
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+			setChannelDetail(data.items[0])
+		);
+		fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+			(data) => setVideos(data?.items)
+		);
+	}, [id]);
+	
+	console.log(channelDetail,videos);
+
+	return <div>{id}</div>;
+};
+
+export default ChannelDetail;
